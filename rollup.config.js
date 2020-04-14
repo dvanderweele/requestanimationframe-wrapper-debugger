24a93712b854e15f4297d802d5fcae7a1e347e47
Dave VanderWeele <weele.me@gmail.com>
2020-04-14 12:26:26 -0500

    Added babel, terser

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,9 @@
+import babel from 'rollup-plugin-babel'
+import resolve from 'rollup-plugin-node-resolve'
+import { terser } from 'rollup-plugin-terser'
+
+const production = !process.env.ROLLUP_WATCH
+
 const prefix = 'dist/bundle.'
 
 export default {
@@ -12,9 +18,19 @@ export default {
       format: 'esm'
     },
     {
-      name: 'YourAwesomePackageNameHere',
+      name: 'RequestanimationframeWrapperDebugger',
+      globals: {
+        rwd: 'RafWD'
+      },
       file: `${prefix}umd.js`,
       format: 'umd'
     }
+  ],
+  plugins: [
+    resolve(),
+    babel({
+      exclude: 'node_modules/**'
+    }),
+    production && terser()
   ]
 }
